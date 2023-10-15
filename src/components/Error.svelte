5b6f65fc0bc3131c4eeb253b2bad4752eaf8bc6b
cheesycod <cheesycod@outlook.com>
2023-10-15 11:37:01 +0530

    fix

--- src/components/Error.svelte
+++ src/components/Error.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <section>
-	<h3 aria-live="polite" class="text-center text-6xl font-semibold p-10">
-		Welcome to Infinity Panel
+	<h3 aria-live="polite" class="text-center text-red-500 text-xl font-semibold mt-4">
+		Error
 	</h3>
-	<p class="mt-3 text-4xl font-semibold text-center text-red-400">{msg}</p>
+	<p class="mt-3 font-semibold text-center text-red-400">{msg}</p>
 </section>
