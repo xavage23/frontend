3923c935af7bcc9ca9ff5ebd58dbf1fef0432741
cheesycod <cheesycod@outlook.com>
2023-10-18 12:39:32 +0530

    remove stuff

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,15 +2,12 @@
 	import InputText from '../../components/inputs/InputText.svelte';
 	import ButtonReact from '../../components/button/ButtonReact.svelte';
 	import { error } from '$lib/toast';
-	import { authState, type AuthState } from '$lib/authState';
+	import type { AuthState } from '$lib/authState';
 	import { goto as gotoOnce } from '$app/navigation';
 	import { Color } from '../../components/button/colors';
 	import { fetchClient } from '$lib/fetch';
 	import { apiUrl } from '$lib/constants';
 	import type { ApiError, UserLogin, UserLoginResponse } from '$lib/generated';
-	import logger from '$lib/logger';
-	import ErrorComponent from '../../components/Error.svelte';
-	import Loading from '../../components/Loading.svelte';
 
 	// Safari needs this patch here
 	let navigating: boolean = false;
@@ -20,7 +17,7 @@
 		return await gotoOnce(url);
 	}
 
-	const checkLogin = async () => {
+	/*const checkLogin = async () => {
 		let authStateData = localStorage.getItem('authState');
 		
 		logger.info("Login", authStateData)
@@ -44,7 +41,7 @@
 		} else {
 			logger.info("Login", "Not redirecting user")
 		}
-	}
+	}*/
 
 	let username: string;
 	let password: string;
@@ -85,51 +82,52 @@
 	};
 </script>
 
-{#await checkLogin()}
+<!--{#await checkLogin()}
 	<Loading msg="Please wait..." />
 {:then data}
 	{#if data}
 		<p class="text-white text-xl">Redirecting you... please wait</p>
 	{/if}
-	<article class="p-4">
-		<h1 class="text-3xl font-semibold">Login</h1>
-		<p class="font-semibold text-lg">
-			In order to login to the game, please input the 'Username' and 'Password' you were given.
-		</p>
-
-		<hr class="my-4" />
-
-		<InputText
-			bind:value={username}
-			id="username"
-			label="Username"
-			placeholder="Enter your username"
-			minlength={1}
-			showErrors={false}
-		/>
-
-		<InputText
-			bind:value={password}
-			id="password"
-			label="Password"
-			placeholder="Enter your password"
-			minlength={1}
-			secret={true}
-			showErrors={false}
-		/>
-
-		<ButtonReact
-			color={Color.Themable}
-			icon={'mdi:login'}
-			text={'Login'}
-			states={{
-				loading: 'Please wait...',
-				success: 'Moving you along...',
-				error: 'Failed to login'
-			}}
-			onClick={login}
-		/>
-	</article>
 {:catch err}
 	<ErrorComponent msg={err?.toString()} />
-{/await}
+{/await}-->
+
+<article class="p-4">
+	<h1 class="text-3xl font-semibold">Login</h1>
+	<p class="font-semibold text-lg">
+		In order to login to the game, please input the 'Username' and 'Password' you were given.
+	</p>
+
+	<hr class="my-4" />
+
+	<InputText
+		bind:value={username}
+		id="username"
+		label="Username"
+		placeholder="Enter your username"
+		minlength={1}
+		showErrors={false}
+	/>
+
+	<InputText
+		bind:value={password}
+		id="password"
+		label="Password"
+		placeholder="Enter your password"
+		minlength={1}
+		secret={true}
+		showErrors={false}
+	/>
+
+	<ButtonReact
+		color={Color.Themable}
+		icon={'mdi:login'}
+		text={'Login'}
+		states={{
+			loading: 'Please wait...',
+			success: 'Moving you along...',
+			error: 'Failed to login'
+		}}
+		onClick={login}
+	/>
+</article>
